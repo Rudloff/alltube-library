4977f997976ed506759d84e74efc0c5726e33217
Pierre Rudloff <contact@rudloff.pro>
2021-02-08 22:03:09 +0100

    Use LoggerAwareTrait Instead of implementing setLogger() ourselves

--- classes/Downloader.php
+++ classes/Downloader.php
@@ -15,15 +15,17 @@ use Alltube\Library\Exception\WrongPasswordException;
 use Alltube\Library\Exception\YoutubedlException;
 use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use Symfony\Component\Process\Process;
 
 /**
  * Class used to call youtube-dl and download videos.
  */
-class Downloader
+class Downloader implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
 
     /**
      * youtube-dl binary path.
@@ -69,11 +71,6 @@ class Downloader
     private $params;
 
     /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
      * Downloader constructor.
      * @param string $youtubedl youtube-dl binary path
      * @param string[] $params youtube-dl parameters
@@ -98,15 +95,6 @@ class Downloader
         $this->avconvVerbosity = $avconvVerbosity;
 
         $this->logger = new NullLogger();
-    }
-
-    /**
-     * @param LoggerInterface $logger
-     * @return void
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
     }
 
     /**
